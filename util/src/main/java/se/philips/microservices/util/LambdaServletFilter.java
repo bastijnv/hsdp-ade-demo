2e378a187785e6aaefeff41988680ab953a5b16e
Bastijn Vissers <bastijn.vissers@philips.com>
2015-12-29 20:07:24 +0100

    initial commit dockerizing the services

---
+++ util/src/main/java/se/philips/microservices/util/LambdaServletFilter.java
@@ -0,0 +1,11 @@
+package se.philips.microservices.util;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+
+public interface LambdaServletFilter extends Filter {
+
+    default public void init(FilterConfig filterConfig) throws ServletException {}
+    default public void destroy() {}
+}
---
+++ util/src/main/java/se/philips/microservices/util/MDCHystrixContextCallable.java
@@ -0,0 +1,35 @@
+package se.philips.microservices.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+public class MDCHystrixContextCallable<K> implements Callable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MDCHystrixContextCallable.class);
+
+    private final Callable<K> actual;
+    private final Map parentMDC;
+
+    public MDCHystrixContextCallable(Callable<K> actual) {
+        LOG.debug("Init MDCHystrixContextCallable...");
+        this.actual = actual;
+        this.parentMDC = MDC.getCopyOfContextMap();
+    }
+
+    @Override
+    public K call() throws Exception {
+        LOG.debug("Call using MDCHystrixContextCallable...");
+        Map childMDC = MDC.getCopyOfContextMap();
+
+        try {
+            MDC.setContextMap(parentMDC);
+            return actual.call();
+        } finally {
+            MDC.setContextMap(childMDC);
+        }
+    }
+}
